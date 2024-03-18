cd913a615cf0c09a7d0ee13bf0ac3aef234409c1
ianhara <ianhara97@gmail.com>
2024-03-18 10:47:56 -0400

    added code to grab five day forecast with no formatting

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,41 @@
-function grabWeather(){
-const apiKey = '917f9c9859212ef07d7a25b9d55a0ea1'
-                
-const city = document.getElementById('city')
-const state = document.getElementById('state')
+const apiKey = '917f9c9859212ef07d7a25b9d55a0ea1';
 
+function grabWeather() {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}`;
 
-fetch(apiUrl)
-//  {
-//   method: 'GET', //GET is the default.
-//   credentials: 'same-origin', // include, *same-origin, omit
-//   redirect: 'follow', // manual, *follow, error
-// }
-  .then(function (response) {
-    console.log(response)
-    return response.json();
-    
-  })
-  .then(function (data) {
-    const lat = data.coord.lat
-    const lon = data.coord.lon
-    console.log(data.data.memes[0].name)
-  });
-
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+            document.getElementById('todaysWeather').innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+            
+            fiveDayForecast(lat, lon);
+        })
+        .catch(function(error) {
+            console.log("Error fetching weather:", error);
+        });
 }
 
-grabWeather()
+function fiveDayForecast(lat, lon) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            
+            document.getElementById('fiveDayForecast').innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+        })
+        .catch(function(error) {
+            console.log("Error fetching forecast:", error);
+        });
+}
